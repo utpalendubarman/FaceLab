0f0310985f02ab3dcde1a954251a06a8606dc9b0
utpalendubarman <47846069+utpalendubarman@users.noreply.github.com>
2024-12-31 19:07:30 +0530

    update

--- app.py
+++ app.py
@@ -40,47 +40,26 @@ def uploadZip():
         obj=CleanDirectories(project_id)
 
         #simulate test response
-        res=[]
-        return res
+        res={}
+        return jsonify(res)
         #return jsonify({'success':True,'project_id':project_id})
     except Exception as e:
         Log("Error",str(e))
         return jsonify({'success':False,'error':'Error creating project folder'})
 
-
-
-@app.route('/configure-faces')
-def configFaces():
-    target='faces/1.jpg'
-
-    targets=['faces/1.jpg','faces/2.jpg','faces/3.jpg','faces/4.jpg','faces/5.jpg','faces/6.jpg']
-    sources=['faces/4.jpg','faces/5.jpg','faces/6.jpg']
-    x,w,y,h=[0,10,0,10]
-    t_x,t_w,t_y,t_h=[0,10,0,10]
-
-    res=[]
-    for target in targets:
-        srcs=[]
-        for source in sources:
-            src={
-                "source":source,
-                "x":x,
-                "w":w,
-                "y":y,
-                "h":h,
-            }
-            srcs.append(src)
-        obj={
-            "target":target,
-            "x":x,
-            "w":w,
-            "y":y,
-            "h":h,
-            "sources":srcs
-        }
-        res.append(obj)
-
-    return jsonify({'Success':res})
+@app.route('/process',methods=['POST'])
+def process():
+    #extract json request
+    data=request.json
+    for img in data:
+        face_path=img.face_path
+        original=img.original
+        source_path=img.source_path
+        x=img.x
+        xw=img.xw
+        y=img.y
+        yh=img.yh
+    return jsonify(data)
 
 @app.route('/<path:filename>')
 def custom_static(filename):
